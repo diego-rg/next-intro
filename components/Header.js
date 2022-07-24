9818c1dde480db27eb50af448474676aee04fee5
eu <diego_thegarden@hotmail.com>
2022-07-24 22:31:47 +0200

    seo test 3

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div>
       <main>
-        <h1 className="text-center font-bold text-5xl p-5">Header Next Test</h1>
+        <h1 className="text-center font-bold text-5xl p-5">
+          Header Intro Next Test
+        </h1>
         <h2 className="text-center font-semibold text-2xl">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the standard dummy text ever since the
