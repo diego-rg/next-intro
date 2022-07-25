ffb24ffe4bd005b949004f59f94bbb6a55e1eee0
eu <diego_thegarden@hotmail.com>
2022-07-25 13:55:49 +0200

    api data usage

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,28 @@
 import Head from "next/head";
+import axios from "axios";
 
-export default function About() {
+//usa inicion de codigo ine, 5 cifras sin ceros del final
+export const getStaticProps = async () => {
+  const { data } = await axios.get(
+    "https://www.el-tiempo.net/api/json/v2/provincias/15/municipios/15013"
+  );
+  return {
+    props: { data },
+  };
+};
+
+export default function About({ data }) {
+  console.log(data);
+
   return (
     <div className="bg-red-800 text-white">
       <Head>
         <title>About Next Intro Test</title>
       </Head>
-      <h3 className="font-bold text-xl">About</h3>
-      <p className="text-sm">blah blah blah</p>
+      <h3>{data.municipio.NOMBRE}</h3>
+      <p>Temperatura: {data.temperatura_actual}</p>
+      <p>Mínima: {data.temperaturas.min}</p>
+      <p>Máxima: {data.temperaturas.max}</p>
     </div>
   );
 }
